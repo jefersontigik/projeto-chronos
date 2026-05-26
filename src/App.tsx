ef43a68e11e74900f6483ea4ba9b3981ca69e740
Jeferson Tigik da Silva <80992765072@senacrs.edu.br>
2026-05-26 10:38:54 -0300

    Create components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
+import {Container} from "./components/Container"
+import {Heading} from "./components/Heading"
+
 import "./styles/global.css"
 import "./styles/theme.css"
-import { Heading } from './components/Heading'
-import { TimerIcon } from 'lucide-react'
 
 export function App(){
     console.log("Olá Mundo!")
     return(
         <>
-            <div className="container">
-                <div className="content">
-                    <Heading>
-                        Olá Mundo! 
-                        <button>
-                            <TimerIcon/>
-                        </button>
-                    </Heading>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Neque nam cum soluta voluptatum fugit magni blanditiis.
-                        Voluptatem, at quam?
-                        Quae aliquid voluptatum ullam et natus sed ipsa ipsam accusantium cum.
-                    </p>  
-                </div>
-            </div>
+            <Container>
+                <Heading>
+                    <p>LOGO</p>
+                </Heading>
+            </Container>
+
+            <Container>
+                <Heading>
+                    <p>MENU</p>
+                </Heading>
+            </Container>
+
+            <Container>
+                <Heading>
+                    <p>FORM</p>
+                </Heading>
+            </Container>
+
+            <Container>
+                <Heading>
+                    <p>FOOTER</p>
+                </Heading>
+            </Container>
         </>
     )
 }
